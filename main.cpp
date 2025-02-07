605935528560cf32f5802fb30aba89823641e171
vagostinia <victoria.agostini@upr.edu>
2025-02-07 17:43:47 +0000

    c2

--- main.cpp
+++ main.cpp
@@ -3,12 +3,39 @@
 // Lab Box Office
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+
 using namespace std; 
 
 int main() {
+    // Display purpose of program
+    cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater." << endl;
+
+    // Adult and children ticket costs + percentage theater keeps
+    const double ADULT_PRICE = 10.00;
+    const double CHILD_PRICE = 6.00;
+    const double THEATER_PERCENTAGE = 0.20;
+
+    // Variables to store sales
+    string movieName;
+    int adultTickets, childTickets;
+
+    // Input
+    cout << "Movie name: ";
+    getline(in, movieName)
+
+    cout << "Adult Tickets Sold: ";
+    cin >> adultTickets;
+
+    cout << "Child Tickets Sold: ";
+    cin >> childTickets;
 
 
 
 
-    
+
+
+
+
 }
